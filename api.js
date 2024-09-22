0884ed4fb9fb4f8d2567561a1a7f781b08ee4fdb
Guilherme Campelo <campelo.guilherme@hotmail.com>
2024-09-21 22:00:10 -0300

    modified:   api.js

--- api.js
+++ api.js
@@ -33,7 +33,7 @@ app.post('/gerarPergunta', async (req, res) => {
     }`;
 
     try {
-        const genAI = new GoogleGenerativeAI({ apiKey });
+        const genAI = new GoogleGenerativeAI(apiKey);
         const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
         console.log(`Recebido: dificuldade = ${dificuldade}, tema = ${tema}`);
 

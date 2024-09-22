15ee7555f10af86ccf6d9a8ed8e3ca05d75749c1
Guilherme Campelo <campelo.guilherme@hotmail.com>
2024-09-21 22:36:19 -0300

    novo

--- api.js
+++ api.js
@@ -13,8 +13,8 @@ app.use(express.json());
 
 // Inicializando a instância do GoogleGenerativeAI com a chave de API
 const apiKey = "AIzaSyBc1V0aD1WeeRfFtcC8stNPFvxMYL7P4O8";
-// const genAI = new GoogleGenerativeAI({ apiKey });
-// const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
+const genAI = new GoogleGenerativeAI(apiKey);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"})
 
 app.post('/gerarPergunta', async (req, res) => {
     const { dificuldade, tema } = req.body;
@@ -33,17 +33,14 @@ app.post('/gerarPergunta', async (req, res) => {
     }`;
 
     try {
-        const genAI = new GoogleGenerativeAI(apiKey);
-        const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
         console.log(`Recebido: dificuldade = ${dificuldade}, tema = ${tema}`);
 
-        const response = await model.generateContent({
-            prompt: promptText,
-            maxTokens: 100, 
-        });
+        // Fazendo a requisição para gerar conteúdo usando generateText
+        const response = await model.generateContent(promptText);
 
         console.log('Resposta da API:', response); // Logando a resposta da API
-        if (response && response.text) {
+        if (response && response.candidates && response.candidates.length > 0) {
+            const resultadoJSON = JSON.parse(response.candidates[0].output);
             return res.json({
                 pergunta: resultadoJSON.question,
                 alternativas: resultadoJSON.answers.map(alt => alt.Text),
@@ -52,8 +49,8 @@ app.post('/gerarPergunta', async (req, res) => {
         } else {
             throw new Error('Resposta inválida da API');
         }  
-        } catch (error) {
-          console.error('Erro na requisição à API:', error);
+    } catch (error) {
+        console.error('Erro na requisição à API:', error);
         return res.status(500).json({ error: 'Erro no servidor', details: error.message });
     }
 });
